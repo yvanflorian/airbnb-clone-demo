95c9fe76659e68664cfa84339f9ef7b789633ea6
yvanflorian <34156361+yvanflorian@users.noreply.github.com>
2021-07-07 19:25:42 +0300

    Backend Adding missing Fields

--- server/src/models/graphql/Listing.ts
+++ server/src/models/graphql/Listing.ts
@@ -1,67 +1,72 @@
 import { IListing, Listing } from "../mongoose/Listing"
 
-interface IAggregation{
-   _id:{
-      country : String,
-      country_code : String
-   }
+interface IAggregation {
+  _id: {
+    country: String
+    country_code: String
+  }
 }
-interface ICountries{
-   country : String,
-   country_code : String
-
+interface ICountries {
+  country: String
+  country_code: String
 }
 
 /**
  * GQL Resolver to fetch only one Listing based on the ID
- * 
+ *
  * @param id Listing ID
  * @returns A listing and Review Document
  */
-export const fetchOneListing = async(id: String) => {
-   console.log("GQL: One Listing Query with ID:",id)
-   try {
-      const one: IListing = await Listing.findById(id)
-      // console.log("We're done!",one)
-      return one      
-   } catch (error) {
-      console.error(error)
-   }
+export const fetchOneListing = async (id: String) => {
+  console.log("GQL: One Listing Query with ID:", id)
+  try {
+    const one: IListing = await Listing.findById(id)
+    // console.log("We're done!",one)
+    return one
+  } catch (error) {
+    console.error(error)
+  }
 }
 /**
  * Check this
  * https://docs.mongodb.com/manual/reference/operator/aggregation/group/#std-label-null-example
  * @returns List of Available Countries
  */
-export const fetchDistinctCountries = async() =>{
-   console.log("[Log] GQL Fetch Distinct Countries")
-   let countries: ICountries[] = []
-   try {
-      const distinctCountry: IAggregation[] = await Listing.aggregate([{
-         $group:{
-            _id:{
-               country: "$address.country",
-               country_code: "$address.country_code"
-            }
-         }
-      }])
-      distinctCountry.map((country)=>countries.push({
-         country: country._id.country,
-         country_code: country._id.country_code
-      }))
+export const fetchDistinctCountries = async () => {
+  console.log("[Log] GQL Fetch Distinct Countries")
+  let countries: ICountries[] = []
+  try {
+    const distinctCountry: IAggregation[] = await Listing.aggregate([
+      {
+        $group: {
+          _id: {
+            country: "$address.country",
+            country_code: "$address.country_code",
+          },
+        },
+      },
+    ])
+    distinctCountry.map((country) =>
+      countries.push({
+        country: country._id.country,
+        country_code: country._id.country_code,
+      })
+    )
 
-      return countries
-   } catch (error) {
-      console.error("GQL Fetching Disting countries",error)
-   }
+    return countries
+  } catch (error) {
+    console.error("GQL Fetching Disting countries", error)
+  }
 }
 
-export const fetchCountryListing = async(code: String)=>{
-   console.log(`[Log] GQL Fetch one Country Related Listings: "${code}"`)
-   try {
-      const countries: IListing[] = await Listing.find({"address.country_code":code}).limit(20)
-      return countries
-   } catch (error) {
-      console.error("GQL Fetch one country Error", error)      
-   }
+export const fetchCountryListing = async (code: String) => {
+  console.log(`[Log] GQL Fetch one Country Related Listings: "${code}"`)
+  try {
+    const countries: IListing[] = await Listing.find({
+      "address.country_code": code,
+    }).limit(5)
+    return countries
+  } catch (error) {
+    console.error("GQL Fetch one country Error", error)
+  }
 }
--- server/src/models/graphql/index.ts
+++ server/src/models/graphql/index.ts
@@ -1,96 +1,148 @@
-import { gql, } from 'apollo-server-express'
+import { gql } from "apollo-server-express"
 import * as dayjs from "dayjs"
 import { GraphQLScalarType, Kind } from "graphql"
 import { merge } from "lodash"
 //
-import { ResolverMap } from '../../types/gqlResolver'
-import { fetchCountryListing, fetchDistinctCountries, fetchOneListing } from './Listing'
+import { ResolverMap } from "../../types/gqlResolver"
+import {
+  fetchCountryListing,
+  fetchDistinctCountries,
+  fetchOneListing,
+} from "./Listing"
 
 export const typeDefs = gql`
-   scalar Date
+  scalar Date
 
-   type Location{
-      type: String,
-      coordinates: [Float],
-      is_location_exact: Boolean
-   }
-   type Country{
-      country: String,
-      country_code: String
-   }
-   type ListingAddress{
-      street: String,
-      suburb: String,
-      government_area: String,
-      market: String,
-      country: String,
-      country_code: String,
-      location: Location
-   }
-   type Listing{
-      _id: String,
-      listing_url: String,
-      name: String,
-      summary: String,
-      space: String,
-      description: String,
-      neighborhood_overview: String,
-      notes: String,
-      transit: String,
-      access: String,
-      interaction: String,
-      house_rules: String,
-      property_type: String,
-      room_type: String,
-      bed_type: String,
-      minimum_nights: String,
-      maximum_nights: String,
-      cancellation_policy: String,
-      last_scraped: Date,
-      calendar_last_scraped: Date,
-      first_review: Date,
-      last_review: Date,
-      accomodates:Int,
-      beds: Int,
-      number_of_reviews: Int,
-      address: ListingAddress
-   }
-   type Query{
-      hello: String,
-      oneListing(id: String): Listing,
-      availableCountries: [Country]
-      countryListings(code: String): [Listing]
-   }
+  type Location {
+    type: String
+    coordinates: [Float]
+    is_location_exact: Boolean
+  }
+  type Country {
+    country: String
+    country_code: String
+  }
+  type ListingAddress {
+    street: String
+    suburb: String
+    government_area: String
+    market: String
+    country: String
+    country_code: String
+    location: Location
+  }
+  type ListingImage {
+    thumbnail_url: String
+    medium_url: String
+    picture_url: String
+    xl_picture_url: String
+  }
+  type Availability {
+    availability_30: Int
+    availability_60: Int
+    availability_90: Int
+    availability_365: Int
+  }
+  type ReviewScore {
+    review_scores_accuracy: Int
+    review_scores_cleanliness: Int
+    review_scores_checkin: Int
+    review_scores_communication: Int
+    review_scores_location: Int
+    review_scores_value: Int
+    review_scores_rating: Int
+  }
+  type Host {
+    host_id: String
+    host_url: String
+    host_name: String
+    host_location: String
+    host_about: String
+    host_thumbnail_url: String
+    host_picture_url: String
+    host_neighbourhood: String
+    host_is_superhost: Boolean
+    host_has_profile_pic: Boolean
+    host_identity_verified: Boolean
+    host_listings_count: Int
+    host_total_listings_count: Int
+    host_verifications: [String]
+  }
+  type Listing {
+    _id: String
+    listing_url: String
+    name: String
+    summary: String
+    space: String
+    description: String
+    neighborhood_overview: String
+    notes: String
+    transit: String
+    access: String
+    interaction: String
+    house_rules: String
+    property_type: String
+    room_type: String
+    bed_type: String
+    minimum_nights: String
+    maximum_nights: String
+    cancellation_policy: String
+    last_scraped: Date
+    calendar_last_scraped: Date
+    first_review: Date
+    last_review: Date
+    accomodates: Int
+    beds: Int
+    bedrooms: Int
+    number_of_reviews: Int
+    bathrooms: Float
+    amenities: [String]
+    price: Float
+    weekly_price: Float
+    monthly_price: Float
+    cleaning_fee: Float
+    extra_people: Float
+    guests_included: Int
+    images: ListingImage
+    host: Host
+    address: ListingAddress
+    availability: Availability
+    review_scores: ReviewScore
+  }
+  type Query {
+    hello: String
+    oneListing(id: String): Listing
+    availableCountries: [Country]
+    countryListings(code: String): [Listing]
+  }
 `
 
-
 const scalars = {
-   Date:new GraphQLScalarType({
-      name: 'Date',
-      description: 'Date custom scalar type',
-      serialize(value) {
-        return dayjs(value).format("DD-MM-YYYY HH:mm:ss")
-      },
-      parseValue(value) {
-        return dayjs(value)
-      },
-      parseLiteral(ast) {
-        if (ast.kind === Kind.STRING) {
-          return dayjs(ast.value)
-       }
-        return null
-      },
-    }),
+  Date: new GraphQLScalarType({
+    name: "Date",
+    description: "Date custom scalar type",
+    serialize(value) {
+      return dayjs(value).format("DD-MM-YYYY HH:mm:ss")
+    },
+    parseValue(value) {
+      return dayjs(value)
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.STRING) {
+        return dayjs(ast.value)
+      }
+      return null
+    },
+  }),
 }
 
-
 const someResolvers: ResolverMap = {
-   Query:{
-      hello : () => "hello World",
-      oneListing: (_,args) => fetchOneListing(args.id),
-      availableCountries : () => fetchDistinctCountries(),
-      countryListings:(_,args) => fetchCountryListing(args.code)
-   }
+  Query: {
+    hello: () => "hello World",
+    oneListing: (_, args) => fetchOneListing(args.id),
+    availableCountries: () => fetchDistinctCountries(),
+    countryListings: (_, args) => fetchCountryListing(args.code),
+  },
 }
 
 export const resolvers = merge(scalars, someResolvers)
