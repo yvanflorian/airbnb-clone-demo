9a530fd70f82c5b2969b4c74910b9ca1b938b0e7
yvanflorian <34156361+yvanflorian@users.noreply.github.com>
2021-08-04 11:08:58 +0300

    TS Node adjustments

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,33 +3,33 @@ import { config } from "dotenv"
 import { ApolloServer } from "apollo-server-express"
 //
 import { dbConnect } from "./config/db"
-import { typeDefs, resolvers} from "./models/graphql"
+import { typeDefs, resolvers } from "./models/graphql"
 
 const loadEnv = config()
-if(loadEnv.error){
-   throw loadEnv.error
+if (loadEnv.error) {
+  throw loadEnv.error
 }
 
-const app  = express()
+const app = express()
 
 const server = new ApolloServer({
-   typeDefs: typeDefs,
-   resolvers: resolvers
+  typeDefs: typeDefs,
+  resolvers: resolvers,
 })
 
-server.applyMiddleware({app})
+server.applyMiddleware({ app })
 
-const startServer = async(port) => {
-   try {
-      await dbConnect()
-      app.listen(port,()=>{
-         console.log(`Server Listening at port ${port}`)
-      })
-      // await fetchDistinctCountries()
-   } catch (error) {
-      console.error(error)
-      process.exit(1)
-   }
+const startServer = async (port: number) => {
+  try {
+    await dbConnect()
+    app.listen(port, () => {
+      console.log(`Server Listening at port ${port}`)
+    })
+    // await fetchDistinctCountries()
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
 }
 
-startServer(process.env.PORT||8000)
+startServer(Number(process.env.PORT) || 8000)
